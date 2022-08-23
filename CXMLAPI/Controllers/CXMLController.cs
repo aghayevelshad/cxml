2fb8ef56b554efaf459bea88ba6f86532c6a92b6
Emil Babazade <emilbabazade29@gmail.com>
2022-08-23 09:43:38 +0400

    Add project files.

---
+++ CXMLAPI/Controllers/CXMLController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace CXMLAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CXMLController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnv;
+
+        public CXMLController(IWebHostEnvironment webHostEnv)
+        {
+            _webHostEnv = webHostEnv;
+        }
+
+        [HttpPost]
+        public IActionResult PostCXMLFile([FromForm] IFormFile file)
+        {
+            string dir = "/Uploads/XML";
+            DateTime now = DateTime.Now;
+            string uploadDirectory = $"{dir}/{now.Year}/{now.Month}/{now.Day}/{now.Hour}";
+            string path = _webHostEnv.ContentRootPath + uploadDirectory;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string fileName = Guid.NewGuid().ToString().Replace("-", "") + file.FileName;
+            using (FileStream stream = new(Path.Combine(path, fileName), FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            string filePath = Path.Combine(path, fileName);
+            return Ok();
+        }
+    }
+}
